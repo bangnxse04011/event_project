d33a62a4932c04ec4b05e190d299962a61dd7a78
bangnx1 <bangnx1@fsoft.com.vn>
2018-04-23 18:29:20 +0700

    update code

--- Event-project/routes/lang.js
+++ Event-project/routes/lang.js
@@ -1,19 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const valid_common = require('../public/javascripts/common/valid_common');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/:lang', function (req, res, next) {
+    // Get lang from URL
     let lang = req.params['lang'];
+    // Get URL in session
     let URL = req.session.CUR_URl;
-    console.log("----------------------------------");
-    console.log(lang + " --------- " + URL);
+    // Valid check case URL null
     if (URL == null || URL == '' || URL == "") {
         URL = 'vi';
     }
-    if (URL.indexOf(lang) <= 0) {
-        URL = URL.replace('en', lang);
-        if (lang.indexOf('vi') > 0) {
-            URL = URL.replace('vi', lang);
+    // Change lang form vi to en or en to vi
+    if (URL.indexOf('/' + lang) <= 0) {
+        if (lang == 'vi') {
+            URL = URL.replace('/en', '/vi');
+        } else {
+            URL = URL.replace('/vi', '/en');
         }
     }
     res.redirect(URL);
