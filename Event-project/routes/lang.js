151a9b234d2f83da423fe611c50a0926f794e4fb
bangnx1 <bangnx1@fsoft.com.vn>
2018-05-09 22:34:46 +0700

    update code

--- Event-project/routes/lang.js
+++ Event-project/routes/lang.js
@@ -7,19 +7,19 @@ router.get('/:lang', function (req, res, next) {
     // Get lang from URL
     let lang = req.params['lang'];
     // Get URL in session
-    // let URL = req.session.CUR_URl;
-    // // Valid check case URL null
-    // if (URL == null || URL == '' || URL == "") {
-    //     URL = '/vi';
-    // }
-    // // Change lang form vi to en or en to vi
-    // if (URL.indexOf('/' + lang) <= 0) {
-    //     if (lang == 'vi') {
-    //         URL = URL.replace('/en', '/vi');
-    //     } else {
-    //         URL = URL.replace('/vi', '/en');
-    //     }
-    // }
+    let URL = req.session.CUR_URl;
+    // Valid check case URL null
+    if (URL == null || URL == '' || URL == "") {
+        URL = '/vi';
+    }
+    // Change lang form vi to en or en to vi
+    if (URL.indexOf('/' + lang) <= 0) {
+        if (lang == 'vi') {
+            URL = URL.replace('/en', '/vi');
+        } else {
+            URL = URL.replace('/vi', '/en');
+        }
+    }
     req.session.CUR_URl = '/' + lang;
     res.redirect('/' + lang);
 });
